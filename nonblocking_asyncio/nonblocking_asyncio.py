472c38e3af40e52e19206f21cb006294230d683b
gdcs92 <gdcs92@gmail.com>
2023-03-23 01:48:48 -0300

    nonblocking_asyncio: implementa chamadas assíncronas
    com a função run_async_funcs_that_may_take_time.

--- nonblocking_asyncio/nonblocking_asyncio.py
+++ nonblocking_asyncio/nonblocking_asyncio.py
@@ -7,11 +7,6 @@ import random
 ITERATION_PERIOD = 2
 
 
-def run_tasks_that_take_some_time():
-    time_to_waste = random.uniform(0, ITERATION_PERIOD/2)
-    sleep(time_to_waste)
-
-
 def fill_iteration_period(start_time: datetime, iteration_period: int):
     now = datetime.now()
     delta_t = (now - start_time).total_seconds()
@@ -26,14 +21,38 @@ def fill_iteration_period(start_time: datetime, iteration_period: int):
     sleep(remaining_time)
 
 
+def run_sync_funcs_that_take_some_time():
+    time_to_waste = random.uniform(0, ITERATION_PERIOD/2)
+    sleep(time_to_waste)
+
+
+async def func_that_may_take_time(task_id) -> None:
+    func_running_time = round(random.uniform(0, 0.1), 4)
+    # sleep(func_running_time)
+    await asyncio.sleep(func_running_time)
+    print(f"* Task {task_id} executada em {func_running_time:.4f} segundos.")
+    return
+
+
+async def run_async_funcs_that_may_take_time():
+    num_tasks = 5
+    tasks = [
+        asyncio.create_task(func_that_may_take_time(i))
+        for i in range(num_tasks)
+    ]
+    return await asyncio.gather(*tasks)
+
+
 async def main():
-    max_iter = 5
+    max_iter = 3
 
     for i in range(1, max_iter+1):
         iteration_start = datetime.now()
         print(f"Iteração {i} iniciada {iteration_start.strftime('%H:%M:%S.%f')}")
 
-        run_tasks_that_take_some_time()
+        run_sync_funcs_that_take_some_time()
+
+        await run_async_funcs_that_may_take_time()
 
         fill_iteration_period(iteration_start, ITERATION_PERIOD)
 
