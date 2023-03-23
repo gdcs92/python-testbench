23248bb85979ef09339674bf18df14656e0a5a56
gdcs92 <gdcs92@gmail.com>
2023-03-23 01:38:15 -0300

    Adiciona nonblocking_asyncio.py
    Implementado estrutura básica das iterações.

---
+++ nonblocking_asyncio/nonblocking_asyncio.py
@@ -0,0 +1,43 @@
+import asyncio
+from datetime import datetime
+from time import sleep
+
+
+def run_tasks_that_take_some_time():
+    pass
+
+
+def fill_iteration_period(start_time: datetime, iteration_period: int):
+    now = datetime.now()
+    delta_t = (now - start_time).total_seconds()
+
+    if delta_t >= iteration_period:
+        return
+
+    remaining_time = iteration_period - delta_t
+    print(
+        f"Dormindo por {remaining_time} segundos para completar o período da iteração"
+    )
+    sleep(remaining_time)
+
+
+async def main():
+    max_iter = 5
+    iteration_period = 2
+
+    for i in range(1, max_iter+1):
+        iteration_start = datetime.now()
+        print(f"Iteração {i} iniciada {iteration_start.strftime('%H:%M:%S.%f')}")
+
+        run_tasks_that_take_some_time()
+
+        fill_iteration_period(iteration_start, iteration_period)
+
+        iteration_end = datetime.now()
+        print(f"Iteração {i} terminada em {iteration_end.strftime('%H:%M:%S.%f')}")
+        print(f"{(iteration_end - iteration_start).total_seconds() = }\n")
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
+    print("Loop principal finalizado.\n")
