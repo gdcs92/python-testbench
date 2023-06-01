0c160a20cf60db7f3e48d114b4852ffdf0c9c32b
gdcs92 <gdcs92@gmail.com>
2023-05-31 22:52:47 -0300

    asyncio_exception_handling: modifica do_something
    Adiciona argumento para ativar/desativar levantamento de exceções
    e modifica condicional para levantar exceções nos casos idx = 1 e
    idx = 3.

--- asyncio_exception_handling/main.py
+++ asyncio_exception_handling/main.py
@@ -1,22 +1,26 @@
 import asyncio
 import time
 
-async def do_something(idx: int) -> float:
+async def do_something(idx: int, raise_exceptions: bool) -> float:
     print(f"{idx = } foi dormir.")
     t1 = time.time()
     await asyncio.sleep(idx)
     t2 = time.time()
     deltat = t2 - t1
 
-    if idx == 3:
+    if idx in [1, 3] and raise_exceptions:
         raise RuntimeError(f"Houve um problema na execução da tarefa {idx}")
 
     print(f"{idx = } acordou após {deltat:.3f} segundos.")
     return deltat
 
 async def main():
+
+    raise_exceptions = True
+
     tasks = [
-        asyncio.create_task(do_something(idx)) for idx in range(1, 5)
+        asyncio.create_task(do_something(idx, raise_exceptions))
+        for idx in range(1, 5)
     ]
     deltas = await asyncio.gather(*tasks, return_exceptions=True)
 
